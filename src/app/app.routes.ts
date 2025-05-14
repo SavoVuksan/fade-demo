ecdbdd922e7655bf2fcd0aaf37f03ca2883dc895
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-14 12:33:33 +0200

    #13: Adds routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,41 @@
 import { Routes } from '@angular/router';
 import { demoDataResolver } from './resolvers/demo-data.resolver';
 import { ScenarioSelectPageComponent } from './pages/scenario-select-page/scenario-select-page.component';
+import { NeuronSelectPageComponent } from './pages/neuron-select-page/neuron-select-page.component';
+import { LabelDetailsPageComponent } from './pages/label-details-page/label-details-page.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'scenario-select',
+        data: {
+            id: 0
+        },
+        title: 'Scenario',
         component: ScenarioSelectPageComponent,
-        resolve: { data: demoDataResolver }
+        resolve: { data: demoDataResolver },
+        children: [
+            {
+                path: 'neuron-select',
+                data: {
+                    id: 1
+                },
+                title: 'Neuron Select',
+                component: NeuronSelectPageComponent,
+                children: [
+                    {
+                        path: 'label-details',
+                        title: 'Label Details',
+                        data: {
+                            id: 2
+                        },
+                        component: LabelDetailsPageComponent
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'scenario-select'
     }
 ];
