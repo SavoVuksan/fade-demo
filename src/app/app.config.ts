4f594993ff09925a46b4e18af2eafcf09002193a
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-14 12:34:06 +0200

    #13: Adds router store to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,15 +7,18 @@ import { provideHttpClient } from '@angular/common/http';
 import { dataReducer } from './state/demo-data.reducer';
 import { provideEffects } from '@ngrx/effects';
 import * as dataEffects from './state/demo-data.effects';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({
-      demoData: dataReducer
+      demoData: dataReducer,
+      router: routerReducer
     }),
     provideHttpClient(),
-    provideEffects(dataEffects)
+    provideEffects(dataEffects),
+    provideRouterStore()
   ]
 };
