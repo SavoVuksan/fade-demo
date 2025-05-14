4eec35ece357eb118b80cbc6a5021ce0dde3e992
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-14 16:34:49 +0200

    #13: Changes routes to be hierarchical & load demo data for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,36 +6,48 @@ import { LabelDetailsPageComponent } from './pages/label-details-page/label-deta
 
 export const routes: Routes = [
     {
-        path: 'scenario-select',
-        data: {
-            id: 0
-        },
-        title: 'Scenario',
-        component: ScenarioSelectPageComponent,
+        path: 'app',
         resolve: { data: demoDataResolver },
         children: [
             {
-                path: 'neuron-select',
+                path: 'scenario-select',
+                data: {
+                    id: 0
+                },
+                title: 'Scenario',
+                component: ScenarioSelectPageComponent,
+                resolve: { data: demoDataResolver },
+            },
+            {
+                path: 'scenario-select/:scenario-id/neuron-select',
                 data: {
                     id: 1
                 },
                 title: 'Neuron Select',
                 component: NeuronSelectPageComponent,
-                children: [
-                    {
-                        path: 'label-details',
-                        title: 'Label Details',
-                        data: {
-                            id: 2
-                        },
-                        component: LabelDetailsPageComponent
-                    }
-                ]
+            },
+            {
+                path: 'scenario-select/:scenario-id',
+                redirectTo: 'scenario-select/:scenario-id/neuron-select'
+            },
+            {
+                path: 'scenario-select/:scenario-id/neuron-select/:neuron-id',
+                title: 'Label Details',
+                data: {
+                    id: 2
+                },
+                component: NeuronSelectPageComponent
+            },
+            {
+                path: 'scenario-select/:scenario-id/neuron-select/:neuron-id/label-details',
+                title: 'Label Details',
+                data: {
+                    id: 2
+                },
+                component: LabelDetailsPageComponent
             }
         ]
-    },
-    {
-        path: '**',
-        redirectTo: 'scenario-select'
     }
+
 ];
+
