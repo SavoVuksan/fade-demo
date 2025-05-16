fe536fdbc6e977d2f4698acbc7a07741e86cd399
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-16 14:30:10 +0200

    #13: Removes LayoutPage & Fixes Header to use actvatedRoute directly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { DemoDataStore } from './state/demo-data.store';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  readonly store = inject(DemoDataStore);
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,60 +2,47 @@ import { Routes } from '@angular/router';
 import { ScenarioSelectPageComponent } from './pages/scenario-select-page/scenario-select-page.component';
 import { NeuronSelectPageComponent } from './pages/neuron-select-page/neuron-select-page.component';
 import { LabelDetailsPageComponent } from './pages/label-details-page/label-details-page.component';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 export const routes: Routes = [
     {
-        path: 'app',
-        redirectTo: 'app/scenario-select',
-        pathMatch: 'full'
+        path: 'scenario-select',
+        data: {
+            id: 0
+        },
+        title: 'Scenario',
+        component: ScenarioSelectPageComponent,
     },
     {
-        path: 'app',
-        component: LayoutPageComponent,
-        children: [
-            {
-                path: 'scenario-select',
-                data: {
-                    id: 0
-                },
-                title: 'Scenario',
-                component: ScenarioSelectPageComponent,
-            },
-            {
-                path: 'scenario-select/:scenario-id/neuron-select',
-                data: {
-                    id: 1
-                },
-                title: 'Neuron Select',
-                component: NeuronSelectPageComponent,
-            },
-            {
-                path: 'scenario-select/:scenario-id',
-                redirectTo: 'scenario-select'
-            },
-            {
-                path: 'scenario-select/:scenario-id/neuron-select/:neuron-id',
-                title: 'Neuron Select',
-                data: {
-                    id: 2
-                },
-                component: NeuronSelectPageComponent
-            },
-            {
-                path: 'scenario-select/:scenario-id/neuron-select/:neuron-id/label-details',
-                title: 'Label Details',
-                data: {
-                    id: 2
-                },
-                component: LabelDetailsPageComponent
-            }
-        ]
+        path: 'scenario-select/:scenario-id/neuron-select',
+        data: {
+            id: 1
+        },
+        title: 'Neuron Select',
+        component: NeuronSelectPageComponent,
+    },
+    {
+        path: 'scenario-select/:scenario-id',
+        redirectTo: 'scenario-select'
+    },
+    {
+        path: 'scenario-select/:scenario-id/neuron-select/:neuron-id',
+        title: 'Neuron Select',
+        data: {
+            id: 1
+        },
+        component: NeuronSelectPageComponent
+    },
+    {
+        path: 'scenario-select/:scenario-id/neuron-select/:neuron-id/label-details',
+        title: 'Label Details',
+        data: {
+            id: 2
+        },
+        component: LabelDetailsPageComponent
     },
     {
         path: '**',
-        redirectTo: 'app/scenario-select'
+        redirectTo: 'scenario-select'
     }
-
 ];
 
