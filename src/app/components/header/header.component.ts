3cb0dcf02465b9ef9102a2783db3aa91fe7d4bbc
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-16 14:50:09 +0200

    #13: Refactors header to use activated route only

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe, } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-header',
