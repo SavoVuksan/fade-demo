b38cb8bd8ecbddb911afb8ea191938c3d9ff2a33
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-16 14:31:38 +0200

    #13: Removes ngrx store devtools dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,9 @@ import { provideRouter, withRouterConfig } from '@angular/router';
 import { inject, provideAppInitializer } from '@angular/core';
 
 import { routes } from './app.routes';
-import { provideStore, Store } from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { DemoDataStore } from './state/demo-data.store';
 
@@ -17,14 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideStore({
       router: routerReducer,
-    }),
-    provideStoreDevtools({ // Todo: Remove when building for production
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-      connectInZone: true // If set to true, the connection is established within the Angular zone
     }),
     provideRouterStore(),
     provideAppInitializer(async () => {
