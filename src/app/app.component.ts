7ac78506a0e1436ce529140dca7b41fb5843fa84
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-19 13:35:59 +0200

    #8: Adds link to legal page from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { DemoDataStore } from './state/demo-data.store';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
