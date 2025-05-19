f7fa5e6660ea77dbc77c60cf885fa40558f52f63
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-19 11:29:50 +0200

    #15: Adds first implementation of neuron select component

--- src/app/pages/neuron-select-page/components/neuron-select/neuron-select.component.ts
+++ src/app/pages/neuron-select-page/components/neuron-select/neuron-select.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { Neuron } from '../../../../models/models';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-neuron-select',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './neuron-select.component.html',
   styleUrl: './neuron-select.component.scss'
 })
 export class NeuronSelectComponent {
+  neurons = input.required<Array<Neuron>>();
+  activeNeuronIds = input.required<Array<number>>();
+
+  transformedNeurons = computed(() => {
+    const neurons = this.neurons();
+    const activateNeuronIds = this.activeNeuronIds()
+    return neurons.map((neuron) => ({
+      ...neuron,
+      isActive: activateNeuronIds.find((id) => id === neuron.id) ? true : false
+    }))
+  })
 
 }
