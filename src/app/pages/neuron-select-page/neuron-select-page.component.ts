576e515bcd647325404f26ec847decd20ac236b5
Savo Vuksan <savo.vuksan@hhi.fraunhofer.de>
2025-05-19 11:53:55 +0200

    #15: Adds first version of activation component

--- src/app/pages/neuron-select-page/neuron-select-page.component.ts
+++ src/app/pages/neuron-select-page/neuron-select-page.component.ts
@@ -1,13 +1,14 @@
 import { AsyncPipe, JsonPipe } from '@angular/common';
-import { Component, effect, inject, Input, InputSignalWithTransform, InputOptionsWithTransform, input } from '@angular/core';
+import { Component, effect, inject, Input, InputSignalWithTransform, InputOptionsWithTransform, input, computed } from '@angular/core';
 import { Params } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DemoDataStore } from '../../state/demo-data.store';
 import { NeuronSelectComponent } from "./components/neuron-select/neuron-select.component";
+import { ActivationComponent } from './components/activation/activation.component';
 
 @Component({
   selector: 'app-neuron-select-page',
-  imports: [AsyncPipe, JsonPipe, NeuronSelectComponent],
+  imports: [AsyncPipe, JsonPipe, NeuronSelectComponent, ActivationComponent],
   templateUrl: './neuron-select-page.component.html',
   styleUrl: './neuron-select-page.component.scss'
 })
@@ -16,6 +17,18 @@ export class NeuronSelectPageComponent {
   readonly scenarioId = input<number>(0, {
     alias: 'scenario-id',
   });
+  readonly neuronId = input<number>(0, {
+    alias: 'neuron-id'
+  })
+
+  readonly selectedNeuron = computed(() => {
+    if (this.neuronId()) {
+      const selectedNeuronId = parseInt(this.neuronId().toString());
+      return this.store.neurons().find((neuron) => neuron.id === selectedNeuronId);
+    } else {
+      return undefined;
+    }
+  })
 
   onScenarioIdChange = effect(() => {
     this.store.changeScenario(this.scenarioId());
